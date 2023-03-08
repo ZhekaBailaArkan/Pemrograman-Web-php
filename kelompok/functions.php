3636b4b0682463e3924e491d2d42cf9057aeea5c
ZhekaBailaArkan <zhekabaila@gmail.com>
2023-03-08 10:59:44 +0700

    menambahkan folder kelompok

---
+++ kelompok/functions.php
@@ -0,0 +1,10 @@
+<?php 
+
+$conn = mysqli_connect("localhost", "root", "", "koperasi_siswa");
+
+function query($query){
+    global $conn;
+    
+}
+
+?>
---
+++ kelompok/output.php
@@ -0,0 +1,74 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "koperasi_siswa");
+
+$query = mysqli_query($conn, "SELECT * FROM tabungan ");
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Output Tabungan</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<style>
+    * {
+        font-family: Helvetica;
+    }
+
+    thead {
+        background-color: darkgrey;
+    }
+
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+</style>
+
+<body>
+
+    <div class="container">
+        <table cellspacing="0" cellpadding="15" border="1">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nama Siswa</th>
+                    <th>NIS</th>
+                    <th>Kelas</th>
+                    <th>Jurusan</th>
+                    <th>Nominal</th>
+                    <th>Tanggal</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $i = 1; ?>
+                <?php while ($row = mysqli_fetch_assoc($query)) : ?>
+                    <tr>
+                        <td><?= $i; ?></td>
+                        <td><?= $row["nama_siswa"]; ?></td>
+                        <td><?= $row["nis"]; ?></td>
+                        <td><?= $row["kelas"]; ?></td>
+                        <td><?= $row["jurusan"]; ?></td>
+                        <td><?= $row["nominal"]; ?></td>
+                        <td><?= $row["tanggal"]; ?></td>
+                    </tr>
+                <?php
+                    $i++;
+                endwhile;
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+</body>
+
+</html>
